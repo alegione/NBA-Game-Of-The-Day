3aa7ba019f95883eed39c6ea2f050d8f6fe97988
alegione <alistair.legione@gmail.com>
2020-01-05 22:32:36 +1100

    adjust package install

--- RunShiny.R
+++ RunShiny.R
@@ -1,4 +1,11 @@
-packages = c("shiny", "jsonlite", "dplyr", "DT", "curl", "httr")
-install.packages(packages, repos = "https://cran.rstudio.com/")
+required_packages = c("shiny", "jsonlite", "dplyr", "DT", "curl", "httr")
+
+packages_to_install = required_packages[!(required_packages %in% installed.packages()[, 1])]
+
+if (length(packages_to_install) > 0) {
+  install.packages(packages_to_install, repos = "https://cran.rstudio.com/")
+}
+
 library(shiny)
+
 runGitHub("NBA-Game-Of-The-Day", "alegione")
